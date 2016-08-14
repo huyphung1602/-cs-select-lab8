fe0a80932a2fc556cbd8e335b8e1f2250cc8d517
Chau Vo <minhchau273@gmail.com>
2016-08-14 23:41:23 +0700

    [f] Drag ball to catch pokemon

--- PokeLab/View Controllers/CatchViewController.swift
+++ PokeLab/View Controllers/CatchViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class CatchViewController: UIViewController {
 
   @IBOutlet weak var pokemonImageView: UIImageView!
+  @IBOutlet weak var ballImageView: UIImageView!
 
   var selectedIndex: Int?
+  var originalBallCenter: CGPoint!
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -22,4 +24,36 @@ class CatchViewController: UIViewController {
     }
   }
 
+  @IBAction func onBallPanned(sender: UIPanGestureRecognizer) {
+    let state = sender.state
+    let translation = sender.translationInView(view)
+
+    switch state {
+    case .Began:
+      originalBallCenter = ballImageView.center
+
+    case .Changed:
+      ballImageView.center = CGPoint(x: originalBallCenter.x + translation.x, y: originalBallCenter.y + translation.y)
+
+    case .Ended:
+      if CGRectIntersectsRect(ballImageView.frame, pokemonImageView.frame) {
+        UIView.animateWithDuration(1, delay: 0, usingSpringWithDamping: 0.1, initialSpringVelocity: 2, options: [], animations: {
+          self.pokemonImageView.transform = CGAffineTransformMakeScale(1.1, 1.1)
+          }, completion: { finished in
+            UIView.animateWithDuration(1, animations: {
+              self.pokemonImageView.center = self.ballImageView.center
+              self.pokemonImageView.transform = CGAffineTransformMakeScale(0.01, 0.01)
+              self.pokemonImageView.alpha = 0
+            })
+        })
+      } else {
+        UIView.animateWithDuration(2, delay: 0, usingSpringWithDamping: 0.1, initialSpringVelocity: 2, options: [], animations: {
+          self.ballImageView.center = self.originalBallCenter
+          }, completion: nil)
+      }
+
+    default:
+      break
+    }
+  }
 }
